d26f7f798cdec787a70adee4a1de052f5f34d285
Abdullah Sajid <77003390+abdullahsajid@users.noreply.github.com>
2026-01-11 14:17:18 +0500

    Update metadata title for project management

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,8 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Advanced Technology Solutions | IT Consulting & Project Management",
-  description: `We provide project management, engineering design, site auditing, specialized industrial solutions, 
-                packaging automation, and compliance excellence across the MENA Region.`,
+  title: "Advanced Technology Solutions | Project Management",
+  description: `We provide project management, engineering design, site auditing, specialized industrial solutions, packaging automation, and compliance excellence across the MENA Region.`,
 };
 
 export default function RootLayout({
